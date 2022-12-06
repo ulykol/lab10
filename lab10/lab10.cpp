f7819bbc56bfc475ec937d76fc4f3d910e0802c6
ulykol <112924408+ulykol@users.noreply.github.com>
2022-12-06 08:45:20 +0300

    lab 9, 10
    lab 9, 10

---
+++ lab10/lab10.cpp
@@ -0,0 +1,124 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+
+int BFS(int** A, int* M, int i, int N) {
+	queue <int> Q;
+	int j;
+	Q.push(i);
+	M[i] = 0;
+	while (Q.empty() != true) {
+		i = Q.front();
+		Q.pop();
+		for (j = 0; j < N; j++) {
+			if ((A[i][j] != 0) && (M[j] > M[i] + A[i][j])) {
+				M[j] = 0;
+				Q.push(j);
+				M[j] = M[i] + A[i][j];
+			}
+		}
+	}
+	return 0;
+}
+
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int N, i, j, C = 0;
+	printf("Ведите размер квадратной матрицы: ");
+	scanf_s("%d", &N);
+	srand(time(NULL));
+	int* M = (int*)malloc(N * sizeof(int));
+	int* EX = (int*)malloc(N * sizeof(int));
+	int** A = (int**)malloc(N * sizeof(int*));
+	for (int i = 0; i < N; i++) {
+		A[i] = (int*)malloc(N * sizeof(int));
+	}
+	printf("\n Матрица смежности для взвешенного неориентированного графа \n\n");
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			A[i][j] = rand() % 10 + 1;
+			if (A[i][j] > 5) {
+				A[i][j] = 0;
+			}
+			A[j][i] = A[i][j];
+			if (i == j) {
+				A[i][j] = 0;
+			}
+		}
+	}
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			printf("%2d  ", A[i][j]);
+		}
+		printf("\n");
+	}
+	for (i = 0; i < N; i++) {
+		M[i] = 1000;
+	}
+	for (i = 0; i < N; i++) {
+		EX[i] = -1;
+	}
+	printf("\n\nПоиск эксцентриситетов :\n");
+	int D = -1, r = 10000;
+	for (i = 0; i < N; i++) {
+		BFS(A, M, i, N);
+		printf("\n");
+
+
+		for (int l = 0; l < N; l++) {
+			printf("%3d ", M[l]);
+		}
+
+		int k;
+		for (k = 0; k < N; k++) {
+			if ((EX[i] < M[k]) && (M[k] != 1000)) {
+				EX[i] = M[k];
+			}
+		}
+		if ((EX[i] < r) && (EX[i] != 0)) {
+			r = EX[i];
+		}
+		if (EX[i] > D) {
+			D = EX[i];
+		}
+		printf("         Эксцентириситет вершины %d :  %d\n", i + 1, EX[i]);
+		for (int l = 0; l < N; l++) {
+			M[l] = 1000;
+		}
+	}
+	printf("\n\nРадиус :  %d\n", r);
+	printf("Диаметр :  %d\n", D);
+	printf("Центральные вершины: ");
+	for (int n = 0; n < N; n++) {
+		if (EX[n] == r) {
+			printf("  %d", n + 1);
+		}
+	}
+	printf("\nПериферийные вершины: ");
+	for (int n = 0; n < N; n++) {
+		if (EX[n] == D) {
+			printf("  %d", n + 1);
+		}
+	}
+	for (i = 0; i < N; i++) {
+		int  step = 0;
+		for (j = 0; j < N; j++) {
+			if (A[i][j] != 0) {
+				step++;
+			}
+		}
+	}
+
+	free(A);
+	free(M);
+	free(EX);
+	_getch();
+}
---
+++ lab9/lab9.cpp
@@ -0,0 +1,134 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale>
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <stack>
+#include <queue>
+#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+
+using namespace std;
+int N, j, i, m;
+int* dist; // дистанция
+int vis[]; //выделяем память под булевы значения для матрицы
+int** graph;
+queue<int>Q; //очередь
+int start;
+
+void BFSD(int vertex, int N, int** graph) { // алгоритм в ширину
+
+	Q.push(vertex); //вносим в очередь вершину
+	dist[vertex] = 0; // обозначаем расстояние до нее
+	while (!Q.empty()) {
+		vertex = Q.front(); // вершину переносим в начало очереди
+		printf(" %d", vertex + 1);
+		Q.pop(); // выносим из очереди
+		for (int i = 0; i < N; i++) {
+			if ((graph[vertex][i] == 1) && (dist[i] == -1)) {
+				Q.push(i);//вносим удовлетворяющую условию вершину в очередь
+				dist[i] = dist[vertex] + 1;//считаем дистанцию до вершины
+			}
+		}
+	}
+}
+
+void DFSD(int vertex, int N, int** graph, bool vis[]) { // алгоритм в глубину
+	vis[vertex] = 1;
+	printf(" %d", vertex + 1);
+	for (int i = 0; i < N; i++)
+	{
+		if ((graph[vertex][i] == 1) && (vis[i] == 0)) {
+			DFSD(i, N, graph, vis);
+		}
+	}
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	srand(time(NULL));
+	int min = 10000;
+	printf("Ведите размер матрицы: ");
+	scanf_s("%d", &N);
+
+	graph = new int* [N];//выделяем память под матрицу
+	dist = new int[N]; // выделяем память для значений дистанции вершин друг от друга
+
+	for (int i = 0; i < N; i++) {
+		graph[i] = new int[N];
+	}
+	for (int i = 0; i < N; i++) { //задаем изначальные значения дистанций
+		dist[i] = -1;
+	}
+
+	printf("\n");
+
+	for (i = 0; i < N; ++i) //заполняем матрицу
+	{
+		for (j = i; j < N; ++j)
+		{
+			graph[i][j] = graph[j][i] = rand() % 2;
+			graph[i][i] = graph[j][j] = 0; // чтобы петля(узел) не создавалась
+		}
+
+	}
+	printf("   ");
+
+	for (j = 0; j < N; j++)
+	{
+		printf("%2d ", j + 1); //горизонтальные нумерация сверху матрицы
+	}
+	printf("\n\n");
+	for (i = 0; i < N; i++) {
+
+		printf(" %d ", i + 1); // вертикальная нумерация слева матрицы
+
+		for (j = 0; j < N; j++)
+		{
+			printf("%2d ", graph[i][j]);
+		}
+		printf("\n\n");
+	}
+
+	printf("\n Введите вершину, с которой нужно начинать обход: ");
+	scanf_s("%d", &start);
+	printf("\n Результат обхода в ширину: ");
+	srand(time(NULL));
+	BFSD(start - 1, N, graph);
+	cout << "\nВремя работы реализаций алгоритмов поиска расстояний на основе обхода в ширину = " << clock() / 1000.0 << endl; // время работы программы
+	printf("\n Вершины: ");
+	printf("   ");
+	for (int i = 1; i <= N; i++) {
+		printf(" %d ", i);
+	}
+	printf("\n Расстояние: ");
+	for (int i = 0; i < N; i++) {
+		printf(" %d ", dist[i]);
+	}
+
+	printf("\n\n");
+
+	printf(" Результат обхода в глубину: ");
+	bool* vis = new bool[start];
+	for (int i = 0; i < N; i++)
+	{
+		vis[i] = { 0 };
+	}
+	srand(time(NULL));
+	DFSD(start - 1, N, graph, vis);
+	cout << "\nВремя работы реализаций алгоритмов поиска расстояний на основе обхода в глубину = " << clock() / 1000.0 << endl; // время работы программы
+	printf("\n Вершины: ");
+	printf("   ");
+	for (int i = 1; i <= N; i++) {
+		printf(" %d ", i);
+	}
+	printf("\n Расстояние: ");
+	for (int i = 0; i < N; i++) {
+		printf(" %d ", dist[i]);
+	}
+	printf("\n\n");
+}
